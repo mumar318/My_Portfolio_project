311d8f8391853fd256a769f7d68f89c0e2bcf8ab
Muhammad Umar <144512204+mumar318@users.noreply.github.com>
2026-04-26 20:52:53 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Only migrate if DATABASE_URL is available (skip during build)
+if [ -n "$DATABASE_URL" ]; then
+    python manage.py migrate
+fi
